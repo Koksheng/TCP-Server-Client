07f3e287ff57442f93bb12e1f7a1463211ef292c
Gue Kok Sheng <gue_kok_sheng@medinno.com>
2022-05-25 00:48:08 +0800

    Initial commit

---
+++ TCP_Client/TCP_Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. create socket
+            Socket tcpClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //2. initiate to build the connection's request
+            IPAddress ipaddress = IPAddress.Parse("192.168.1.86");
+            EndPoint point = new IPEndPoint(ipaddress, 7788);
+            tcpClient.Connect(point);//via ip & port number, locate a server to connect
+            byte[] data = new byte[1024];
+            int length = tcpClient.Receive(data); // byte[], to receive the data
+            //length return how many byte of data its return;
+            string message = Encoding.UTF8.GetString(data, 0, length); //convert received data into string
+            Console.WriteLine(message);
+
+
+            //send msg to server
+            string messge2 = Console.ReadLine(); //read user input
+            tcpClient.Send(Encoding.UTF8.GetBytes(messge2)); //convert string to Byte[] then send to server
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TCP_Server/TCP_Server/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. create socket
+            Socket tcpServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //2. bind ip and port number 192.168.1.86
+            IPAddress ipaddress = new IPAddress(new byte[] { 192,168,1,86});
+            EndPoint point = new IPEndPoint(ipaddress,7788);
+            tcpServer.Bind(point);//Apply to the operating system for an available ip and port number for communication
+            //3. Start to listen (Waiting for client to connect)
+            tcpServer.Listen(100);
+            Console.WriteLine("start listening");
+
+            Socket clientSocket = tcpServer.Accept(); //pause the current thread, until next client connected, then execute the code below
+
+            Console.WriteLine("Client connected");
+            //using return socket to communicate with client
+            string message = "hello 欢迎您";
+            byte[] data = Encoding.UTF8.GetBytes(message); //convert string into byte array
+            clientSocket.Send(data);
+            Console.WriteLine("Sent msg to client");
+
+            byte[] data2 = new byte[1024]; // create a byte array as container
+            int length = clientSocket.Receive(data2); //receive data from client
+            string message2 = Encoding.UTF8.GetString(data2,0,length); // convert byte[] to string
+
+            Console.WriteLine("Message from client "+ message2);
+            Console.ReadKey();
+
+        }
+    }
+}
